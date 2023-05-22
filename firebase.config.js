90b4efbffd654202915e794d7b999bbc0ccaa7eb
Vu Dao <anhvudao17@gmail.com>
2023-05-22 23:40:42 +0300

    index firebase config

---
+++ firebase.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: "AIzaSyA6-28z6q4E8-rAJnw00RqbGGANdKxISCo",
+  authDomain: "wedding-photos-377305.firebaseapp.com",
+  projectId: "wedding-photos-377305",
+  storageBucket: "wedding-photos-377305.appspot.com",
+  messagingSenderId: "358857594765",
+  appId: "1:358857594765:web:06182c3b6a7daf7e918fc4",
+  measurementId: "G-T04HKZ0VTX",
+};
